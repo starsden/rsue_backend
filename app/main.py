feafd603acd0b494b66c945021fcf3e712fa43c1
starsden <159563356+starsden@users.noreply.github.com>
2025-11-08 19:29:32 +0300

    ✔️ add cors for dev

--- app/main.py
+++ app/main.py
@@ -3,8 +3,19 @@ from app.routers.router import router
 from app.routers.orga_rt import orga
 from app.routers.sklads import sklad
 from app.core.core import engine, Base
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="RSUE Backend", description="rsue.devoriole.ru", docs_url="/", version="0.2.1")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 Base.metadata.create_all(bind=engine)
 app.include_router(router)
 app.include_router(orga)
